b1b7640c6071c7acd9284e68cb65cb0507e44a32
Francisco González Rodríguez <paco_glez02@hotmail.com>
2023-12-13 23:49:06 -0600

    Configuracion inicial

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+//Src: sirve para identificar el archivo
+//Dest: para guardar el archivo
+//Pipe: compila
+const { src, dest, watch } = require("gulp"); //Apunta al devDependencies del package.json
+const sass = require('gulp-sass')(require('sass')); //Importamos
+
+
+function css(done) {
+    src('src/scss/app.scss')//Identificar el archivo SASS 
+        .pipe(sass())//Compilar    
+        .pipe( dest("build/css")); //Almacenar en disco duro
+    
+    done();//Callback avisando a Gulp cuando llego el final
+}
+
+function dev(done) {//Funcion de desarrollo para el watch
+    watch("src/scss/app.scss", css);//Cuando cambie la ruta especificada, manda llamar la func css
+    
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
